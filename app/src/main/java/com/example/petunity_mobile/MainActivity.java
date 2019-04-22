7ebce47366d8d0f7a5a42d3697a017ae74c309e7
monteiro-pro <monteiro_pro@hotmail.com>
2019-04-21 23:28:52 -0300

    Conclusão da Tela Inicial.

--- app/src/main/java/com/example/petunity_mobile/MainActivity.java
+++ app/src/main/java/com/example/petunity_mobile/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.petunity_mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        (findViewById(R.id.btDaor)).setOnClickListener(btnDaorAtion);
+        (findViewById(R.id.btAdotar)).setOnClickListener(btnAdotarAtion);
     }
+
+   /* Button btnSend = (Button) findViewById(R.id.btDaor);
+
+btnSend.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //faça algo
+        }
+    }; */
+
+    View.OnClickListener btnDaorAtion= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            }
+        };
+
+    View.OnClickListener btnAdotarAtion= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
